b005f9ffe7d4f236603a13b48ddb432ac2733788
Harold Modesto <harold.modesto@chromedia.com>
2012-07-17 17:05:39 +0800

    Initial bundle configuration

---
+++ ChromediaMediaBundle.php
@@ -0,0 +1,28 @@
+<?php
+namespace Chromedia\MediaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+// use Chromedia\MediaBundle\DependencyInjection\Compiler\AddProviderCompilerPass;
+
+class ChromediaMediaBundle extends Bundle 
+{
+//     /**
+//      * {@inheritdoc}
+//      */
+//     public function build(ContainerBuilder $container)
+//     {
+//         $container->addCompilerPass(new AddProviderCompilerPass());
+//     }
+
+//     /**
+//      * {@inheritdoc}
+//      */
+//     public function boot()
+//     {
+//         // this is required by the AWS SDK (see: https://github.com/knplabs/Gaufrette)
+//         if (!defined("AWS_CERTIFICATE_AUTHORITY")) {
+//             define("AWS_CERTIFICATE_AUTHORITY", true);
+//         }
+//     }
+}
