f01524d6ad1a07abbbfd1763a081bcb2d1af8f2b
Harold Modesto <harold.modesto@chromedia.com>
2012-07-18 16:15:07 +0800

    typo in namespace

--- DependencyInjection/MainConfiguration.php
+++ DependencyInjection/MainConfiguration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Knp\Bundle\GaufretteBundle\DependencyInjection;
+namespace Chromedia\Bundle\MediaBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
